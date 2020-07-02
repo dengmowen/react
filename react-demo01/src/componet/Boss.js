4e96f77b018cd977fbe3ca51ebedb824c71fa7c2
dengmw <dengmw@dengmwdeMacBook-Pro.local>
2020-07-02 18:38:55 +0800

    [add]新增CSSTransition

--- react-demo01/src/componet/Boss.js
+++ react-demo01/src/componet/Boss.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+import { CSSTransition } from "react-transition-group";
 class Boss extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      isShow: true
-     }
-     this.handleChange=this.handleChange.bind(this);
+    this.state = {
+      isShow: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
-  render() { 
+  render() {
     return (
-      <div>
-        <div className={this.state.isShow? 'show':'hide'}>Boss级人物-孙悟空</div>
-    <button onClick={this.handleChange}>{this.state.isShow ? '隐藏': '召唤'}Boss</button>
-      </div>
+      //   <div>
+      //     <div className={this.state.isShow? 'show':'hide'}>Boss级人物-孙悟空</div>
+      // <button onClick={this.handleChange}>{this.state.isShow ? '隐藏': '召唤'}Boss</button>
+      //   </div>
+      <Fragment>
+        <CSSTransition
+          in={this.state.isShow}
+          timeout={2000}
+          classNames="boss-text"
+          unmountOnExit
+        >
+          <div>Boss级人物-孙悟空</div>
+        </CSSTransition>
+        <div>
+          <button onClick={this.handleChange}>
+            {this.state.isShow ? "隐藏" : "召唤"}Boss
+          </button>
+        </div>
+      </Fragment>
     );
   }
-  handleChange(){
+  handleChange() {
     this.setState({
-      isShow: !this.state.isShow
-    })
+      isShow: !this.state.isShow,
+    });
   }
 }
- 
 export default Boss;
